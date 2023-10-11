1d92f7005ca25a04945b380f27342c3861206fa6
kolacik33 <kolacik33@pm.me>
2023-10-11 13:22:35 +0000

    added list tests

---
+++ tests/dip/test_lists.py
@@ -0,0 +1,17 @@
+import sys, os
+import pytest
+import numpy as np
+sys.path.insert(0, 'src')
+
+from scinumtools.dip import DIP
+from scinumtools.dip.datatypes import FloatType
+
+def test_node_list():
+    with DIP() as p:
+        p.from_file("examples/definitions.dip")
+        env = p.parse()
+        
+    assert env.nodes.keys()                       == ['box', 'modules', 'runtime']
+    assert env.nodes['runtime'].keys()            == ['t_max', 'timestep']
+    assert env.nodes['runtime']['timestep'].value == FloatType(1.0000000000000001e-11, 's')
+    assert env.nodes['runtime.t_max'].value       == FloatType(1e-08, 's')
