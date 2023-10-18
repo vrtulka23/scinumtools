af19e5d652b70f62ea2cdc5c1573a93a43eba45f
kolacik33 <kolacik33@pm.me>
2023-10-18 12:07:31 +0000

    added profiling to banchmark

--- tests/units/test_benchmark.py
+++ tests/units/test_benchmark.py
@@ -4,6 +4,8 @@ from math import isclose
 import os
 import sys
 import time
+import cProfile
+import pstats
 sys.path.insert(0, 'src')
 
 from scinumtools.units import *
@@ -31,4 +33,17 @@ def test_arrays():
         r = i
     ttotal = time.time() - tstart
     assert ttotal < 3.e-1
+
+def test_profiling():
+    
+    with cProfile.Profile() as pr:
+        
+        q = Quantity(np.linspace(1,100,40), 'cm')
+    
+        for i in range(100):
+            r = q.to('au')
+        
+    stats = pstats.Stats(pr)
+    stats.sort_stats(pstats.SortKey.TIME)
+    stats.print_stats()
     
