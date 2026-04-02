d6a6395b3a96c60d517e729b594f9559079a920f
vrtulka23 <vrtulka23@pm.me>
2026-04-02 23:02:11 +0200

    added tests of examples

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -7,7 +7,7 @@ import subprocess
 
 @pytest.fixture
 def test_path():
-    return "examples/"
+    return "examples"
 
 def test_examples_n_body(test_path):
 
@@ -31,4 +31,20 @@ def test_examples_elmag_force(test_path):
     result = subprocess.run([
         "python3", "simulation.py", "--init", "--docs"
     ],capture_output=True, text=True, cwd=dirTest)
-    
+
+    # check if files were created
+    assert os.path.isfile(f"{dirTest}/config.h")
+    assert os.path.isfile(f"{dirTest}/documentation.pdf")
+
+def test_examples_dip_example(test_path):
+
+    # set path to the example
+    dirTest = f"{test_path}/dip_example"
+
+    # run example
+    result = subprocess.run([
+        "python3", "main.py"
+    ],capture_output=True, text=True, cwd=dirTest)
+
+    # test the output
+    assert result.stdout == "Number of spheres: Quantity(2.450e+02)\nTotal mass:        Quantity(1.275e+01 kg)\n"
